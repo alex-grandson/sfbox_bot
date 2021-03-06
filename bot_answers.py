ed66b31117614fc1fc5c5c62fa2148d08ed394ec
alex-grandson <70079537+alex-grandson@users.noreply.github.com>
2021-03-06 18:28:37 +0300

    oaoaoaoa

---
+++ bot_answers.py
@@ -0,0 +1,2 @@
+def greetings(name):
+    return f'Hello, {name}! I\'m SFBox helper bot.\n\nYou can send me files with data and I will send you back the output!'
--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import config
-print(config.Token)
+import bot_answers
+import telebot
+
+bot = telebot.TeleBot(config.Token, parse_mode=None)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, bot_answers.greetings(message.from_user.first_name))
+
+
+@bot.message_handler(func=lambda message: message.document.mime_type == 'text/plain', content_types=['document'])
+def handle_text_doc(message):
+    print("yes")
+
+
+bot.polling()
